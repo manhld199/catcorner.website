3f950d437a8bb25a0346412befb97ce3996717a0
ldmnh <leducmanh123kt@gmail.com>
2024-10-19 19:28:28 +0700

    style(global): fix breakpoint

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -66,12 +66,11 @@ const config: Config = {
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
       },
-    },
-    screens: {
-      phone: "320px",
-      tablet: "768px",
-      laptop: "1024px",
-      desktop: "1440px",
+      screens: {
+        mm: "320px",
+        ml: "425px",
+        phone: "320px",
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],
