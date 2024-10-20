d71f17e05ba1482e56afda047f36ea6ec2e76d16
Thuy Trinh ne <21522719@gm.uit.edu.vn>
2024-10-20 12:52:30 +0700

    feat(auth): fix vercel p1

--- src/app/(auth)/register/verify-email/page.tsx
+++ src/app/(auth)/register/verify-email/page.tsx
@@ -1,17 +1,11 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import {
-	Card,
-	CardContent,
-	CardFooter,
-	CardHeader,
-	CardTitle,
-} from "@/components/ui/card";
+import { Card, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Mail } from "lucide-react";
-
 import { useRouter, useSearchParams } from "next/navigation";
 import { useEffect, useState, Suspense, CSSProperties } from "react";
+import EmailContent from "./EmailContent";
 
 export default function RegisterVerifyEmail() {
 	const [email, setEmail] = useState("");
@@ -58,27 +52,5 @@ export default function RegisterVerifyEmail() {
 				</Button>
 			</CardFooter>
 		</Card>
-	);
-}
-
-interface EmailContentProps {
-	email: string;
-}
-
-function EmailContent({ email }: EmailContentProps) {
-	return (
-		<>
-			<CardContent className="text-left">
-				<p className="text-gray-600 mb-4">
-					Vui lòng xác minh địa chỉ email của bạn để hoàn tất việc đăng ký.
-					Chúng tôi đã gửi một email xác nhận tới:
-				</p>
-				<p className="text-green-500 font-semibold mb-4 text-center">{email}</p>
-				<p className="text-sm text-gray-500">
-					Nhấn &quot;Gửi lại email&quot; nếu bạn không nhận được bất kỳ email
-					nào trong vòng 10 phút.
-				</p>
-			</CardContent>
-		</>
 	);
 }
