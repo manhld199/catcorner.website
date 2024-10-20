9f7cd8ab0c847c8bf7d64b2cd0398b6ac095e280
Thuy Trinh ne <21522719@gm.uit.edu.vn>
2024-10-20 13:00:39 +0700

    feat(auth): Fix Suspense boundary

--- src/app/(auth)/register/verify-email/page.tsx
+++ src/app/(auth)/register/verify-email/page.tsx
@@ -1,13 +1,19 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { Card, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+	Card,
+	CardContent,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
 import { Mail } from "lucide-react";
+
 import { useRouter, useSearchParams } from "next/navigation";
 import { useEffect, useState, Suspense, CSSProperties } from "react";
-import EmailContent from "./EmailContent";
 
-export default function RegisterVerifyEmail() {
+function VerifyEmailContent() {
 	const [email, setEmail] = useState("");
 	const [isLoading, setIsLoading] = useState(true);
 	const router = useRouter();
@@ -36,9 +42,17 @@ export default function RegisterVerifyEmail() {
 					XÁC MINH EMAIL CỦA BẠN
 				</CardTitle>
 			</CardHeader>
-			<Suspense fallback={<div>Loading...</div>}>
-				<EmailContent email={email} />
-			</Suspense>
+			<CardContent className="text-left">
+				<p className="text-gray-600 mb-4">
+					Vui lòng xác minh địa chỉ email của bạn để hoàn tất việc đăng ký.
+					Chúng tôi đã gửi một email xác nhận tới:
+				</p>
+				<p className="text-green-500 font-semibold mb-4 text-center">{email}</p>
+				<p className="text-sm text-gray-500">
+					Nhấn &quot;Gửi lại email&quot; nếu bạn không nhận được bất kỳ email
+					nào trong vòng 10 phút.
+				</p>
+			</CardContent>
 
 			<CardFooter className="flex flex-col gap-8 pt-11">
 				<Button className="w-full mx-auto block text-xl" variant="filled">
@@ -52,5 +66,13 @@ export default function RegisterVerifyEmail() {
 				</Button>
 			</CardFooter>
 		</Card>
+	);
+}
+
+export default function VerifyEmailPage() {
+	return (
+		<Suspense>
+			<VerifyEmailContent />
+		</Suspense>
 	);
 }
