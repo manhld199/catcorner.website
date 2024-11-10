87b8f46dfee859de1a5c49025b50c012552d9234
ldmnh <leducmanh123kt@gmail.com>
2024-11-10 17:13:24 +0700

    fix(admin/coupons): fix deploy

--- src/app/admin/coupons/add/page.tsx
+++ src/app/admin/coupons/add/page.tsx
@@ -7,22 +7,11 @@ import FormBody from "../form-body";
 // import data
 import { PAGE_DATA } from "@/data/admin";
 
-// import utils
-import { ADMIN_PRODUCTS_URL, ADMIN_USERS_URL } from "@/utils/constants/urls";
-import { fetchData } from "@/utils/functions/server";
-
 export default async function AdminGroupAddPage() {
-  const productData = await fetchData(ADMIN_PRODUCTS_URL);
-  const userData = await fetchData(ADMIN_USERS_URL);
-
   return (
     <div className="w-full flex flex-col justify-center items-center gap-6">
       <h1 className="w-fit">{PAGE_DATA["group-add"]}</h1>
-      <FormBody
-        data={[]}
-        productData={productData.products}
-        userData={userData.users}
-      />
+      <FormBody data={[]} />
     </div>
   );
 }
