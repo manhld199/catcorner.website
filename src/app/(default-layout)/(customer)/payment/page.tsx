85dd22d873d4128e06ddcc042a4806c266df51d9
ldm <leducmanh123kt@gmail.com>
2025-01-27 19:57:48 +0700

    feat(payment): fix handle payment

--- src/app/(default-layout)/(customer)/payment/page.tsx
+++ src/app/(default-layout)/(customer)/payment/page.tsx
@@ -47,7 +47,7 @@ export default function PaymentPage() {
             {
               method: "POST",
               headers: { "Content-Type": "application/json" },
-              body: JSON.stringify(paymentData),
+              body: JSON.stringify({ ...paymentData, mobile: false }),
             }
           );
 
