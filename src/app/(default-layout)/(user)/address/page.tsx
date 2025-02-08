7ff12a5caddc5ebd55b637f8e5f2402fc62af99a
Thuy trinh ne <21522719@gm.uit.edu.vn>
2025-02-08 21:43:24 +0700

    feat(address) update redirect after adding address

--- src/app/(default-layout)/(user)/address/page.tsx
+++ src/app/(default-layout)/(user)/address/page.tsx
@@ -4,20 +4,30 @@ import { Card, CardContent } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
 import { PenLine, Trash2, PlusCircle } from "lucide-react";
 import { motion } from "framer-motion";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import { useState, useEffect } from "react";
 import { useSession } from "next-auth/react";
 import { USER_URL } from "@/utils/constants/urls";
 import { Address } from "@/types/address";
 import Swal from "sweetalert2";
 import withReactContent from "sweetalert2-react-content";
+import { toast, ToastContainer } from "react-toastify";
 
 const MySwal = withReactContent(Swal);
 
 export default function DeliveryAddress() {
   const router = useRouter();
+  const searchParams = useSearchParams();
+
   const { data: session, status } = useSession();
   const [addresses, setAddresses] = useState<Address[]>([]);
+  useEffect(() => {
+    if (searchParams.get("success") === "true") {
+      toast.success("Địa chỉ đã được thêm thành công!");
+      router.replace("/address");
+    }
+  }, [searchParams, router]);
+
   const fetchAddresses = async () => {
     if (!session?.user?.accessToken) return;
     try {
@@ -94,75 +104,80 @@ export default function DeliveryAddress() {
   };
 
   return (
-    <div className="rounded-xl border bg-card text-card-foreground shadow w-[100%] dark:bg-black dark:border-gray-700">
-      <div className="w-full l p-4 space-y-4">
-        <div className="flex justify-between items-center mb-6">
-          <h2 className="font-bold dark:text-white">Địa chỉ nhận hàng</h2>
-          <Button
-            onClick={handleAddNewAddress}
-            variant="filled"
-            className="relative">
-            <PlusCircle className="h-4 w-4 mr-2" />
-            Thêm địa chỉ mới
-          </Button>
-        </div>
-        <div className="space-y-4 mx-auto w-full">
-          {addresses.map((address, index) => (
-            <motion.div
-              key={address._id}
-              initial={{ opacity: 0, y: 20 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{ duration: 0.3, delay: index * 0.1 }}>
-              <Card
-                className={`relative ${address.is_default ? "border-2 border-pri-7 border-dashed" : ""} transition-all duration-300 ease-in-out hover:shadow-md`}>
-                <CardContent className="p-4">
-                  <div className="flex justify-between items-start gap-4">
-                    <div className="space-y-2 flex-1">
-                      <div className="flex items-center gap-2">
-                        <span className="font-medium">{address.full_name}</span>
-                        <span className="text-muted-foreground">
-                          - {address.phone}
-                        </span>
-                        {address.is_default && (
-                          <Badge
-                            variant="default"
-                            className="bg-pri-7 text-primary-foreground">
-                            Mặc định
-                          </Badge>
-                        )}
+    <>
+      <div className="rounded-xl border bg-card text-card-foreground shadow w-[100%] dark:bg-black dark:border-gray-700">
+        <div className="w-full l p-4 space-y-4">
+          <div className="flex justify-between items-center mb-6">
+            <h2 className="font-bold dark:text-white">Địa chỉ nhận hàng</h2>
+            <Button
+              onClick={handleAddNewAddress}
+              variant="filled"
+              className="relative">
+              <PlusCircle className="h-4 w-4 mr-2" />
+              Thêm địa chỉ mới
+            </Button>
+          </div>
+          <div className="space-y-4 mx-auto w-full">
+            {addresses.map((address, index) => (
+              <motion.div
+                key={address._id}
+                initial={{ opacity: 0, y: 20 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.3, delay: index * 0.1 }}>
+                <Card
+                  className={`relative ${address.is_default ? "border-2 border-pri-7 border-dashed" : ""} transition-all duration-300 ease-in-out hover:shadow-md`}>
+                  <CardContent className="p-4">
+                    <div className="flex justify-between items-start gap-4">
+                      <div className="space-y-2 flex-1">
+                        <div className="flex items-center gap-2">
+                          <span className="font-medium">
+                            {address.full_name}
+                          </span>
+                          <span className="text-muted-foreground">
+                            - {address.phone}
+                          </span>
+                          {address.is_default && (
+                            <Badge
+                              variant="default"
+                              className="bg-pri-7 text-primary-foreground">
+                              Mặc định
+                            </Badge>
+                          )}
+                        </div>
+                        <p className="text-sm text-muted-foreground">
+                          {address.province.name}, {address.district.name},{" "}
+                          {address.ward.name}, {address.detail_address}
+                        </p>
                       </div>
-                      <p className="text-sm text-muted-foreground">
-                        {address.province.name}, {address.district.name},{" "}
-                        {address.ward.name}, {address.detail_address}
-                      </p>
-                    </div>
-                    <div className="flex gap-2">
-                      <Button
-                        variant="ghost"
-                        size="sm"
-                        onClick={() => handleUpdateAddress(address._id)}
-                        className="text-primary hover:text-primary/90 transition-colors duration-200">
-                        <PenLine className="h-4 w-4 mr-1" />
-                        Chỉnh sửa
-                      </Button>
-                      {!address.is_default && (
+                      <div className="flex gap-2">
                         <Button
                           variant="ghost"
                           size="sm"
-                          onClick={() => handleDelete(address)}
-                          className="text-destructive hover:text-destructive/90 transition-colors duration-200">
-                          <Trash2 className="h-4 w-4 mr-1" />
-                          Xóa
+                          onClick={() => handleUpdateAddress(address._id)}
+                          className="text-primary hover:text-primary/90 transition-colors duration-200">
+                          <PenLine className="h-4 w-4 mr-1" />
+                          Chỉnh sửa
                         </Button>
-                      )}
+                        {!address.is_default && (
+                          <Button
+                            variant="ghost"
+                            size="sm"
+                            onClick={() => handleDelete(address)}
+                            className="text-destructive hover:text-destructive/90 transition-colors duration-200">
+                            <Trash2 className="h-4 w-4 mr-1" />
+                            Xóa
+                          </Button>
+                        )}
+                      </div>
                     </div>
-                  </div>
-                </CardContent>
-              </Card>
-            </motion.div>
-          ))}
+                  </CardContent>
+                </Card>
+              </motion.div>
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+      <ToastContainer />
+    </>
   );
 }
